21c7e7223fac5f11d980b6e0fa0b11229e82a155
kb-zomato <kanishka.bansal@zomato.com>
2024-06-24 13:40:48 +0530

    integrating trufflehog

--- backend/danswer/utils/masking.py
+++ backend/danswer/utils/masking.py
@@ -1,33 +1,32 @@
-from detect_secrets import SecretsCollection
-from detect_secrets.settings import default_settings
+import json
+import subprocess
 import tempfile
 import os
 from danswer.utils.logger import setup_logger
+from trufflehog3.core import load_rules, DEFAULT_RULES_FILE
+from trufflehog3.search import search
+from trufflehog3.models import File
 
 logger = setup_logger()
 
 def find_and_mask_secrets(text):
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(text.encode())
-        tmp_file_path = tmp_file.name
+   return find_and_mask_by_trufflehog(text)
 
-    # Scan the temporary file
-    with default_settings() as settings:
-        secrets = SecretsCollection()
-        secrets.scan_file(tmp_file_path)
-    
-    # Read back the secrets and mask them
+def find_and_mask_by_trufflehog(text):
+    file = File(path="temp", content=text)
+    issues = search(file, rules=load_rules(DEFAULT_RULES_FILE))
     masked_text = text
     secret_values: list[str] = []
-    for secret in secrets:
-        secret_value = secret[1].secret_value
+    for issue in issues:
+        secret_value = issue.secret
         secret_values.append(secret_value)
         masked_text = masked_text.replace(secret_value, '[MASKED]')
     
     if len(secret_values) > 0:
         logger.info(f"Found and masked {secret_values} in {text}")
-                    
-    # Clean up the temporary file
-    os.remove(tmp_file_path)
     
     return masked_text
+
+
+if __name__ == "__main__":   
+    print(find_and_mask_secrets("password = 'abcdefghijklmnopqrstuvwxyz'"))
